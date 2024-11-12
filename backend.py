d77d6eae8ba9036457336aac22df9dd363914132
ioanni <ioannisgiannopoulosloyola@gmail.com>
2024-11-11 20:56:57 -0600

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,8 @@
+def add_helper():
+    pass
+
+def remove_helper():
+    pass
+
+def predict_helper():
+    pass
---
+++ cache.py
@@ -0,0 +1,8 @@
+def add_helper(user, ticker):
+    pass
+
+def remove_helper(user, ticker):
+    pass
+
+def select_helper(user, ticker, database):
+    pass
